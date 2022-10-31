11263641183c26e9e19b10c7ed2e2bd0d64f0fef
piano-man <reen.gagan@gmail.com>
2022-10-30 23:21:18 -0400

    Test

--- crypto_engine.c
+++ crypto_engine.c
@@ -43,8 +43,8 @@ static int default_setup_cipher(quicly_crypto_engine_t *engine, quicly_conn_t *c
         char hexbuf[PTLS_MAX_DIGEST_SIZE * 2 + 1];
         ptls_hexdump(hexbuf, secret, hash->digest_size);
         printf("GAGAN: Secret when exporting secrets is %s\n", hexbuf);
-        memcpy(secret+(hash->digest_size), aead->name, strlen(aead->name));
-        memcpy(secret+(hash->digest_size)+strlen(aead->name), &epoch, sizeof(size_t));
+        memcpy((char*)secret+(hash->digest_size), aead->name, strlen(aead->name));
+        memcpy((char*)secret+(hash->digest_size)+strlen(aead->name), &epoch, sizeof(size_t));
         send_to_iokernel(secret, hash->digest_size+strlen(aead->name)+sizeof(size_t));
     }
     uint8_t hpkey[PTLS_MAX_SECRET_SIZE];
