d81c7944e11f744279748be293e8b5e96431f230
piano-man <reen.gagan@gmail.com>
2022-11-18 20:01:07 -0500

    Changes

--- crypto_engine.h
+++ crypto_engine.h
@@ -1,3 +1,4 @@
 #include <quicly.h>
 
 extern quicly_crypto_engine_t custom_crypto_engine; 
+size_t quicly_decode_decrypted_packet(quicly_context_t *ctx, quicly_decoded_packet_t *packet, const uint8_t *datagram, size_t datagram_size, size_t *off);
